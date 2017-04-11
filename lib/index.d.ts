c9bfbd62c1b8c5b00a454823b83937c976349342
Philip Lopez <philip.lopez@philiplopez.com>
2017-04-11 13:01:00 +1000

    added subtract, length, normalise, unitDirection

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -4,3 +4,8 @@ export interface Vector2D {
 }
 export declare function scale(factor: number, vector: Vector2D): Vector2D;
 export declare function add(left: Vector2D, right: Vector2D): Vector2D;
+export declare function subtract(left: Vector2D, right: Vector2D): Vector2D;
+export declare function length({x, y}: Vector2D): number;
+export declare function normalise(v: Vector2D): Vector2D;
+export declare const normalize: typeof normalise;
+export declare function unitDirection(from: Vector2D, to: Vector2D): Vector2D;
--- src/index.ts
+++ src/index.ts
@@ -16,3 +16,28 @@ export function add(left : Vector2D, right : Vector2D) : Vector2D {
         y: left.y + right.y
     }
 }
+
+export function subtract(left : Vector2D, right : Vector2D) : Vector2D {
+    return {
+        x: left.x - right.x,
+        y: left.y - right.y
+    }
+}
+
+export function length({x, y} : Vector2D) {
+    return Math.sqrt(x * x + y * y);
+}
+
+export function normalise(v : Vector2D) {
+    // FIXME: test this, especially for divide-by-zero...
+    const vLength = length(v);
+    return (vLength === 0)
+        ? v
+        : scale(1.0 / vLength, v);
+}
+export const normalize = normalise; // alias
+
+export function unitDirection(from : Vector2D, to : Vector2D) {
+    const direction = subtract(to, from);
+    return normalise(direction);
+}
