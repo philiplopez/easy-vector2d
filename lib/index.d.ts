a10efd36853eb700242791ec2b6d2e8538a698bf
Philip Lopez <philip.lopez@philiplopez.com>
2017-04-11 13:13:26 +1000

    built

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -5,6 +5,7 @@ export interface Vector2D {
 export declare function scale(factor: number, vector: Vector2D): Vector2D;
 export declare function add(left: Vector2D, right: Vector2D): Vector2D;
 export declare function subtract(left: Vector2D, right: Vector2D): Vector2D;
+export declare function distance(from: Vector2D, to: Vector2D): number;
 export declare function length({x, y}: Vector2D): number;
 export declare function normalise(v: Vector2D): Vector2D;
 export declare const normalize: typeof normalise;
